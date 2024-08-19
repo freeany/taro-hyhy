ae7ca5e3721291a07a1855b05190c00c9b064301
李浩然 <lhr_freeany@163.com>
2024-08-19 10:03:15 +0800

    环境变量

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -2,7 +2,10 @@ import { View, Text } from '@tarojs/components'
 import { useLoad } from '@tarojs/taro'
 import './index.scss'
 
+
 export default function Category() {
+  const title = process.env.TARO_APP_TITLE
+  const api = process.env.TARO_APP_API
 
   useLoad(() => {
     console.log('Page loaded.')
@@ -11,6 +14,7 @@ export default function Category() {
   return (
     <View className='category page-container'>
       <Text>Hello Category!</Text>
+      <Text>{ title } -- { api }</Text>
       <Text className='iconfont icon-tuikuan a'></Text>
     </View>
   )
